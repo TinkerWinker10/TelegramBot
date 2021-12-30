681a14c3c7d86c759afde7e4b335bbc423195b1c
TinkerWinker10 <51273310+TinkerWinker10@users.noreply.github.com>
2021-12-30 18:32:17 +0200

    Add files via upload

--- weather.py
+++ weather.py
@@ -1,31 +1,27 @@
 import requests 
-from pprint import pprint
+
 from config import *
 
-
-class Weather:
-    def __init__(self, units = "metric"):
-        self.units = units
-
     
-    def current_weather(self, q: str =  "Chicago", appid: str = APPID) -> dict:#текущяя погода
+def current_weather(q: str =  "Chicago", appid: str = APPID, units: str = "metric") -> dict:#текущяя погода
         data =  requests.get(URL_BASE+"weather", params=locals()).json()
-        print(URL_BASE)
-        return data
-  
+        res = None
+        for sub in data["weather"]:
+            if sub["main"]:
+                res = sub
+                break
+        return f'Location: {data["name"]}  Temperature: {data["main"]["temp"]}  Weather status: {res["main"]}'
 
-    def weather_forecast(self, q: str = "Chicago", appid: str = APPID) ->dict:#прогноз погоды
+def weather_forecast(q: str = "Chicago", appid: str = APPID, units: str = "metric", cnt: str = "8") ->dict:#прогноз погоды
         data = requests.get(URL_BASE+"forecast", params=locals()).json()
-        return data
+        for item in data["list"]:
+            value = data["list"]
+        return "\n".join(list(map(str, value)))
 
-
-    def weather_onecall(self, lat: float = 55.68, lon: float = 12.57, appid: str = APPID) -> dict:#json по локации
-        data = requests.get(URL_BASE+"onecall", params=locals()).json()
-        return data
 
 
 
 if __name__ == "__main__":
-    x = Weather("metric")
     location = input("Enter a location: ").strip()
-    pprint(x.weather_forecast(location))
+
+    print(weather_forecast(location))
