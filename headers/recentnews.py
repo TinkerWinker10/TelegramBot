78e0e0e934793715215a24cde365f974a80e6ab7
TinkerWinker10 <51273310+TinkerWinker10@users.noreply.github.com>
2022-01-04 15:45:45 +0200

    Add files via upload

--- headers/recentnews.py
+++ headers/recentnews.py
@@ -7,11 +7,24 @@ from news import *
 
 @dp.message_handler(lambda message: message.text == "🥇Tribuna🥇")
 async def weather_news(message: types.Message):
-    await message.answer(get_sport_news())
+    await message.answer(get_tribuna_news())
 
 
 @dp.message_handler(lambda message: message.text == "📍TSN📍")
 async def weather_news(message: types.Message):
     await message.answer(get_tsn_news())
+
+
+@dp.message_handler(lambda message: message.text == "🗞BBC🗞")
+async def weather_news(message: types.Message):
+    await message.answer(get_bbc_news())
+
+
+@dp.message_handler(lambda message: message.text == "⚽️Sport.ua⚽️")
+async def weather_news(message: types.Message):
+    await message.answer(get_sport_news())
+
+
+
 
 
