84b3dcd46b33895ce676a2100b99113f3834d186
[Your <grgrey25@gmail.com>
2022-01-06 00:21:26 +0200

    Some fixes and new features

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 APPID = '2f6df4e456d2fa62cf6d19b557ee5356'
 URL_WEATHER = "http://api.openweathermap.org/data/2.5/"
-API_TOKEN = "5022736748:AAHXG306uVq0Fl6t0EORIJzwSEEj7jgF2q4"
+API_TOKEN = "2139720619:AAFyeGhcmsjsJD1T94hqlLh4_miZUvBfEQc"
 URL_EXCHANGE = 'https://api.exchangerate.host/latest'
